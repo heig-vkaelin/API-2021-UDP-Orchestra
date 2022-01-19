76ef9da27c982d6e8f3a431496216c172574444c
Valentin Kaelin <kalanehd@gmail.com>
2022-01-19 11:11:48 +0100

    feat: musician looks like it's working

---
+++ docker/image-auditor/src/protocol.js
@@ -0,0 +1,19 @@
+// Multicast address
+const HOST = '239.255.22.5';
+
+// Multicast port
+const PORT = 4242;
+
+const INSTRUMENTS = {
+  piano: 'ti-ta-ti',
+  trumpet: 'pouet',
+  flute: 'trulu',
+  violin: 'gzi-gzi',
+  drum: 'boum-boum',
+};
+
+module.exports = {
+  HOST,
+  PORT,
+  INSTRUMENTS,
+};
--- docker/image-musician/src/protocol.js
+++ docker/image-musician/src/protocol.js
@@ -1,12 +1,19 @@
-/*
- * Our application protocol specifies the following default multicast address and port
- */
+// Multicast address
+const HOST = '239.255.22.5';
 
-const PROTOCOL_MULTICAST_ADDRESS = '239.255.22.5'
-const PROTOCOL_PORT = 9907
+// Multicast port
+const PORT = 4242;
+
+const INSTRUMENTS = {
+  piano: 'ti-ta-ti',
+  trumpet: 'pouet',
+  flute: 'trulu',
+  violin: 'gzi-gzi',
+  drum: 'boum-boum',
+};
 
 module.exports = {
-  PROTOCOL_MULTICAST_ADDRESS,
-  PROTOCOL_PORT
-}
-
+  HOST,
+  PORT,
+  INSTRUMENTS,
+};
